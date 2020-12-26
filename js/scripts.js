c8a785968fa74831ff9037e2f5d16683945ddf25
zulkarnen29 <zulkarnen2912@gmail.com>
2020-12-26 14:14:28 +0700

    update triger for android mobile

--- js/scripts.js
+++ js/scripts.js
@@ -54,31 +54,58 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 
-$(window).scroll(function() {
-    var wScroll = $(this).scrollTop();
+// $(window).scroll(function() {
+//     var wScroll = $(this).scrollTop();
 
-    if (wScroll > 1100 && wScroll < 1737) {
-        $('#myVideo1').trigger('play')
-        console.log(wScroll); 
+//     if (wScroll > 1100 && wScroll < 1737) {
+//         $('#myVideo1').trigger('play')
+//         console.log(wScroll); 
         
-    } else {
-        $('#myVideo1').trigger('pause')
-        console.log ("ini wScroll : " +wScroll)
+//     } else {
+//         $('#myVideo1').trigger('pause')
+//         console.log ("ini wScroll : " +wScroll)
     
-    }
-});
+//     }
+// });
+
+// function myFunction(x) {
+//   if (x.matches) { // If media query matches
+//     document.body.style.backgroundColor = "yellow";
+//   } else {
+//     document.body.style.backgroundColor = "pink";
+//   }
+// }
+
+// var x = window.matchMedia("(max-width: 700px)")
+// myFunction(x) // Call listener function at run time
+// x.addListener(myFunction) // Attach listener function on state changes
+
 
 function myFunction(x) {
-  if (x.matches) { // If media query matches
-    document.body.style.backgroundColor = "yellow";
-  } else {
-    document.body.style.backgroundColor = "pink";
+  if (x.matches) { 
+
+    $(window).scroll(function(){
+        var wScroll = $(this).scrollTop();
+
+        if (wScroll > 1000 && wScroll < 1700) {
+            $('#myVideo1').trigger('play')
+            console.log("oOKOKOKOK")
+        } else {
+            $('#myVideo1').trigger('pause')
+        }
+    });
+
+
+
+
+
   }
 }
 
 var x = window.matchMedia("(max-width: 700px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
+
 
 })(jQuery); // End of use strict
     // // Collapse Navbar
