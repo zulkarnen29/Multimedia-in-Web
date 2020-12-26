08e82a84a586ce0145d6179abadedee03b8d29bc
zulkarnen29 <zulkarnen2912@gmail.com>
2020-12-26 13:47:51 +0700

    update js/

--- js/scripts.js
+++ js/scripts.js
@@ -68,6 +68,17 @@ $(window).scroll(function() {
     }
 });
 
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.body.style.backgroundColor = "yellow";
+  } else {
+    document.body.style.backgroundColor = "pink";
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
 
 })(jQuery); // End of use strict
     // // Collapse Navbar
